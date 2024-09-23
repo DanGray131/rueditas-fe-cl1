d0e829431b3f3bb49137609a1e3633d50bd8abc7
Emanuel <emanueljlv@gmail.com>
2024-09-22 20:43:24 -0500

    Carga inicial

---
+++ src/test/java/pe/edu/cibertec/ruedistasfrontend/Cl1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.ruedistasfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
